ebef1828c8661acefb2aa2d1f42e929d88bcf46c
Harald <harald.pfifer@tu-dresden.de>
2024-11-21 11:29:24 +0100

    started adding local function for structured synthesis

--- LPVutil/lpvmixsyn.m
+++ LPVutil/lpvmixsyn.m
@@ -1,5 +1,4 @@
 function [K,CL,GAM,INFO] = lpvmixsyn(G,varargin)
-
 % LPVMIXSYN Mixed-sensitivity synthesis for PSS
 %
 % lft(P,K): [z1] = [W1 0 ][S  -SG ][W2 0 ][w1]
@@ -8,10 +7,10 @@ function [K,CL,GAM,INFO] = lpvmixsyn(G,varargin)
 % CL: closed loop, feedback connection of G and K with additional performance
 % inputs and outputs. inputs={r,d}, outputs={e,u,y}
 %
-% [K,CL,GAM,INFO]=mixsyn(G,W1,W2,W3,W4,...) calculates the controller K by
+% [K,CL,GAM,INFO]=lpvmixsyn(G,W1,W2,W3,W4,...) calculates the controller K by
 % minimising the induces L2 norm of lft(P,K) as depicted above 
 %
-% [K,CL,GAM,INFO]=mixsyn(G,W1,W2,W3,W4,Xb,Yb) calculates the controller K by
+% [K,CL,GAM,INFO]=lpvmixsyn(G,W1,W2,W3,W4,Xb,Yb) calculates the controller K by
 % minimising the induces L2 norm of lft(P,K) as depicted above. Xb and Yb
 % are BASIS objects, which describe the assumed parameter 
 % dependence of the lyapunov matrices used in solving for K.
@@ -51,17 +50,32 @@ end
 
 % checks
 if ~isa(Wd,'double')
-    error('W4 must be a scalar (type double)')
+    error('W4 must be static (type double)')
 end
 
 % generalised plant
 Pgen = [We*Wr, -We*G*Wd, -We*G; zeros(ncon,ncon+nmeas), Wu; Wr, -G*Wd, -G];
 
 % perform synthesis
-[K,GAM,INFO] = lpvsyn(Pgen,nmeas,ncon,Xb,Yb);
+[K,GAM,INFO] = lpvsyn(Pgen,nmeas,ncon,varagin{5:end});
 
 % inputs: r, d 
 % outputs: e, u, y
 CL = [eye(nmeas) -G; K -K*G; G*K, G];
+
+end
+
+function [K,CL,GAM,INFO] = LOCALlpvmixsynstruc(G,W1,W2,W3,W4,Xb,Yb,opts)
+% helper function to run the structured (observer based) mixed sensitivity
+% problem (see Theis, Pfifer, "Observer‐based synthesis of linear
+% parameter‐varying mixed sensitivity controllers", RNC 2020)
+
+if ~isa(W2,'double')
+    error('W2 must be static (type double)')
+end
+
+% scale plant for coprime factorization
+Gcop = W2\G*W4;
+
 
 end
